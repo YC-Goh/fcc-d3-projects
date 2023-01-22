269225612f95237ab72d2c8b1a01e3a5c9ed86f4
Goh Yeow Chong <gohyc1993@gmail.com>
2023-01-22 22:05:07 +0800

    heat map all tests passing

--- heat-map/src/index.js
+++ heat-map/src/index.js
@@ -20,7 +20,9 @@ const heatPlot = (parentSelector, dataSet, canvasProps) => {
         plotWidth: PLOTWIDTH, 
         plotHeight: PLOTHEIGHT,
         title: PLOTTITLE,
-        titleStyle: PLOTTITLESTYLE
+        titleStyle: PLOTTITLESTYLE,
+        description: PLOTDESCRIPTION,
+        descriptionStyle: PLOTDESCRIPTIONSTYLE
     } = canvasProps;
     const PADDINGX = (CANVASWIDTH - PLOTWIDTH) / 2;
     const MINYEAR = d3.min(newDataSet.map((d) => d.year));
@@ -44,15 +46,63 @@ const heatPlot = (parentSelector, dataSet, canvasProps) => {
     canvas.append('g').call(axisY).attr('transform', `translate(${PADDINGX}, 0)`).attr('id', 'y-axis');
     const heatBoxes = canvas.selectAll('rect').data(newDataSet).enter().append('rect').attr('class', 'cell');
     heatBoxes
-        .attr('data-year', (d) => d.year)
-        .attr('data-month', (d) => d.month)
+        .attr('data-year', (d) => d.year.getFullYear())
+        .attr('data-month', (d) => d.month.getMonth())
         .attr('data-temp', (d) => d.temperature);
     heatBoxes
         .attr('width', CELLWIDTH)
         .attr('height', CELLHEIGHT)
-        .attr('x', (d,i,n) => scaleX(new Date(n[i].getAttribute('data-year'))))
-        .attr('y', (d,i,n) => scaleY(new Date(n[i].getAttribute('data-month'))) - 0.5 * CELLHEIGHT)
-        .attr('fill', (d,i,n) => scaleColour(n[i].getAttribute('data-temp')));
+        .attr('x', (d) => scaleX(d.year))
+        .attr('y', (d) => scaleY(d.month) - 0.5 * CELLHEIGHT)
+        .attr('fill', (d) => scaleColour(d.temperature));
+    heatBoxes
+        .on('mousemove', (e,d) => {
+            heatBoxes.attr('fill', 'lightgray');
+            e.currentTarget.setAttribute('fill', scaleColour(d.temperature));
+            const TARGETX = e.currentTarget.getAttribute('x');
+            const TARGETY = e.currentTarget.getAttribute('y');
+            canvas.select('#tooltip').remove();
+            const tooltip = canvas.append('text').attr('x', TARGETX).attr('y', TARGETY).attr('id', 'tooltip');
+            tooltip
+                .attr('data-year', d.year.getFullYear())
+                .attr('data-month', d.month.getMonth())
+                .attr('data-temp', d.temperature);
+            tooltip
+                .append('tspan')
+                .attr('dx', '2em')
+                .attr('dy', '1em')
+                .text(`DATE: ${d3.timeFormat('%B')(d.month)} ${d3.timeFormat('%Y')(d.year)}`);
+            tooltip
+                .append('tspan')
+                .attr('x', TARGETX)
+                .attr('dx', '2em')
+                .attr('dy', '1em')
+                .text(`TEMPERATURE: ${d3.format('.3f')(d.temperature)}`);
+        });
+    heatBoxes
+        .on('mouseleave', (e,d) => {
+            heatBoxes.attr('fill', (d,i,n) => scaleColour(d.temperature))
+            canvas.select('#tooltip').remove();
+        });
+    const description = main.append('p').text(PLOTDESCRIPTION).attr('style', PLOTDESCRIPTIONSTYLE).attr('id', 'description');
+    const legend = canvas.append('g').attr('id', 'legend');
+    legend
+        .attr('x', scaleX(MINYEAR))
+        .attr('y', 3)
+        .attr('width', PLOTWIDTH)
+        .attr('height', PADDINGY - 6);
+    const legendMarkers = legend.selectAll('rect').data([0,1,2,3,4].map((i) => MINTEMP + (MAXTEMP - MINTEMP) * (i / 4))).enter().append('rect').attr('class', 'legend-marker');
+    legendMarkers
+        .attr('width', PADDINGY - 24)
+        .attr('height', PADDINGY - 24)
+        .attr('x', (d,i) => scaleX(d3.interpolateDate(MINYEAR, MAXYEAR)(i/5)))
+        .attr('y', 12)
+        .attr('fill', (d) => scaleColour(d));
+    const legendTitles = legend.selectAll('text').data([0,1,2,3,4].map((i) => MINTEMP + (MAXTEMP - MINTEMP) * (i / 4))).enter().append('text').attr('id', 'legend-title');
+    legendTitles
+        .attr('x', (d,i) => scaleX(d3.interpolateDate(MINYEAR, MAXYEAR)(i/5+.05)))
+        .attr('y', PADDINGY - 18)
+        .text((d) => `${d3.format('.3f')(d)}`);
 };
 
 let responseObject;
@@ -74,8 +124,10 @@ request.onload = () => {
                 plotWidth: 800,
                 plotHeight: 450,
                 style: 'border: 1px solid black; display: block; margin: 0 auto; padding: 0',
-                title: 'Mean Earth Temperature (Celcius) by Year and Month',
-                titleStyle: 'display: block; width: fit-content; margin: 0 auto; padding: 0'
+                title: 'Mean Earth Temperature (Celsius) by Year and Month',
+                titleStyle: 'display: block; width: fit-content; margin: 0 auto; padding: 0',
+                description: 'Figure 1: Heat map of monthly mean surface temperature of the Earth from ~1750 to ~2010. Mouse over each cell to see its temperature (Celsius).',
+                descriptionStyle: 'display: block; width: fit-content; margin: 0 auto; padding: 0'
             }
         );
     };
